36a73a8eb5ed282bc1300897fb689144f40bc5b0
clement coste <clement.coste.rouet@free.fr>
2021-02-19 14:24:06 +0100

    modification

--- incendie.py
+++ incendie.py
@@ -1,1 +1,12 @@
+#########################################
+# groupe MPCI 7
+# Clement Coste
+# Tom Le Goueff
+# Yanis Kbayli
+# Maeva Laurence
+# https://github.com/uvsq-info/l1-python
+#########################################
 
+import tkinter as tk
+canvas = 
+racine = tk.Tk()
